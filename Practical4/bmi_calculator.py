e7f9da70793466fa6ec1c8941f332e83ae87227a
xyz114514-doge <yingzhe.24@intl.zju.edu.cn>
2025-03-14 11:39:47 +0800

    edit

--- Practical4/bmi_calculator.py
+++ Practical4/bmi_calculator.py
@@ -2,9 +2,9 @@
 #   Store a person's weight(kg)in a variable weight and their height(m) in a variable height.
 #   Then calculate BMI using the formula BMI = weight/(height ** 2).
 #   Use conditional statements to classify BMI:
-# - If BMI > 30, consider the person "obese";
-# - If BMI < 18.5, consider the person "underweight";
-# - Otherwise, the person is considered to be in the "normal" range.
+#   If BMI > 30, consider the person "obese";
+#   If BMI < 18.5, consider the person "underweight";
+#   Otherwise, the person is considered to be in the "normal" range.
 #   Output, which contains the calculated BMI and corresponding health class.
 weight = float(input('How many kilograms do you weigh？') ) 
 height = float(input('What is your height in meters?'))# get data
